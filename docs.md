00e66fd924bae84569adcb6c60c44e1e1a8d51b5
Madman10K <stanv.contact176@gmail.com>
2025-10-26 17:18:57 +0200

    Try to add docs to site

--- docs.md
+++
@@ -1,2 +0,0 @@
-# Coming soon!
-The pkggen repository is not yet public. This page will be filled once it is released to the public.
---
+++ generate-docs.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+for file in docs/*.md; do
+    echo -e "\n\n---\n" >> "$file"
+    cat _Sidebar.md >> "$file"
+done
+find ./ -type f -exec sed -i -E 's/https:\/\/github\.com\/MadLadSquad\/(.*)\/wiki/https:\/\/pkggen\.madladsquad\.com\/docs\/\1/g' {} \;
+
