26b72f5198be91b8a446f021a0af000af3a70254
Stanislav Vasilev <contact@madladsquad.com>
2025-10-26 22:55:33 +0200

    Update generate-docs.sh

--- generate-docs.sh
+++ generate-docs.sh
@@ -5,5 +5,6 @@ for file in *.md; do
     echo -e "\n\n---\n" >> "$file"
     cat _Sidebar.md >> "$file"
 done
+cp Home.md ../docs.md
 find ./ -type f -exec sed -i -E 's/https:\/\/github\.com\/MadLadSquad\/pkggen\/wiki/https:\/\/pkggen\.madladsquad\.com\/docs\//g' {} \;
 cd ..
